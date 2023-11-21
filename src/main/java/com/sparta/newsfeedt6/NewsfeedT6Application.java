87a13d555a44ee98c3e4a2fa2657dbb4c193a386
Robbie <alstjd2627@naver.com>
2023-11-21 19:45:08 +0900

    init commit

---
+++ src/main/java/com/sparta/newsfeedt6/NewsfeedT6Application.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedt6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedT6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsfeedT6Application.class, args);
+	}
+
+}
