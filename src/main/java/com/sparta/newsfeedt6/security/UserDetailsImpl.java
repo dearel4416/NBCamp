c9d485769dfbb570eec1b1fbc33d8b2f1686d4e7
dmlal <opkl8781@naver.com>
2023-11-23 08:42:54 +0900

    feat : Access Token, Refresh Token 구현
    아직까진 잘 동작하고 있음

---
+++ src/main/java/com/sparta/newsfeedt6/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package com.sparta.newsfeedt6.security;
+
+import com.sparta.newsfeedt6.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // 인터페이스라서 반드시 구현해야합니다.  하지만 jwt액세스토큰으로 인증, 인가를 하기 때문에 보안상 비밀번호를 받아올 필요가 없습니다.
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    // 인터페이스라서 반드시 구현해야합니다.  하지만 프로젝트 구현사항에서 사용자 관리자 권한을 구분하지 않았기 때문에 모두의 권한을 통일할 예정입니다.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+
+}
