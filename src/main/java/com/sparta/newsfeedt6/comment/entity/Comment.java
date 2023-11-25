19a91a151b3bda6fe4b211ab6997e46df35184e7
dmlal <opkl8781@naver.com>
2023-11-25 14:02:42 +0900

    feat : 댓글 작성, 수정, 삭제 구현

---
+++ src/main/java/com/sparta/newsfeedt6/comment/entity/Comment.java
@@ -0,0 +1,43 @@
+package com.sparta.newsfeedt6.comment.entity;
+
+import com.sparta.newsfeedt6.entity.PostEntity;
+import com.sparta.newsfeedt6.entity.TimeEntity;
+import com.sparta.newsfeedt6.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(name = "comments")
+@NoArgsConstructor
+public class Comment extends TimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comments_id")
+    private Long id;
+
+    @Column
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    public Comment(String content, User user, PostEntity post) {
+        this.content = content;
+        this.user = user;
+        this.post = post;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
