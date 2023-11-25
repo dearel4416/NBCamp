e1b38a851c995f74fa8fe8bccde934a55d957bac
dmlal <opkl8781@naver.com>
2023-11-25 14:07:26 +0900

    fix : 시큐리티 에러 수정.
    비밀번호 null 대신 아무값이나 리턴
    
    권한 빈 리스트 대신 ROLE_USER로 통일

--- src/main/java/com/sparta/newsfeedt6/security/UserDetailsImpl.java
+++ src/main/java/com/sparta/newsfeedt6/security/UserDetailsImpl.java
@@ -1,18 +1,24 @@
 package com.sparta.newsfeedt6.security;
 
 import com.sparta.newsfeedt6.user.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
 public class UserDetailsImpl implements UserDetails {
+
     private final User user;
 
     public UserDetailsImpl(User user) {
         this.user = user;
     }
+
+
 
     public User getUser() {
         return user;
@@ -26,13 +32,19 @@ public class UserDetailsImpl implements UserDetails {
     // 인터페이스라서 반드시 구현해야합니다.  하지만 jwt액세스토큰으로 인증, 인가를 하기 때문에 보안상 비밀번호를 받아올 필요가 없습니다.
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     // 인터페이스라서 반드시 구현해야합니다.  하지만 프로젝트 구현사항에서 사용자 관리자 권한을 구분하지 않았기 때문에 모두의 권한을 통일할 예정입니다.
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities(){
-        return Collections.emptyList();
+        String authority = "ROLE_USER";
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
     }
 
     @Override
