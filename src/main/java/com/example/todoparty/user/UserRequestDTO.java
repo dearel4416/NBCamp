ac86a075951147d2f742b6e26c62276eb2e571ea
dearel4416 <dearel4416@gmail.com>
2023-12-03 19:18:09 +0900

    UserRequestDTO 테스트

--- src/main/java/com/example/todoparty/user/UserRequestDTO.java
+++ src/main/java/com/example/todoparty/user/UserRequestDTO.java
@@ -9,6 +9,6 @@ public class UserRequestDTO {
     @Pattern(regexp = "^[a-z0-9]{4,10}$")
     private String username;
 
-    @Pattern(regexp = "^[a-zA-z0-9]{8,15}$")
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
     private String password;
 }
