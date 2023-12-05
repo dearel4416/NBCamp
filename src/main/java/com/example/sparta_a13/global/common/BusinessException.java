4942ce4186b00fecae493be516b2738796c20909
oneseel <seno1006@gmail.com>
2023-12-05 15:57:50 +0900

    feat : 예외처리

---
+++ src/main/java/com/example/sparta_a13/global/common/BusinessException.java
@@ -0,0 +1,19 @@
+package com.example.sparta_a13.global.common;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final int status;
+
+  public BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.status = errorCode.getStatus();
+  }
+
+  public BusinessException(ErrorCode errorCode, Throwable cause) {
+    super(errorCode.getMessage(), cause);
+    this.status = errorCode.getStatus();
+  }
+}
