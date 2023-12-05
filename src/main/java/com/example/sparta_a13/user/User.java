3f5e3307a2978d125f92830c253f53b08998a8c0
dearel4416 <dearel4416@gmail.com>
2023-12-05 19:42:53 +0900

    USER FILE OUTLINE

---
+++ src/main/java/com/example/sparta_a13/user/User.java
@@ -0,0 +1,26 @@
+package com.example.sparta_a13.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/sparta_a13/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.sparta_a13.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<String, String> {
+
+}
